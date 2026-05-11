9e7d711b23e80b785626eedd87ab3dfc4332b939
FanderBlue <fanderblue05@gmail.com>
2026-05-11 23:21:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    print("✅ البوت يعمل...")
+   print("🚀 NEW VERSION WORKING 🚀")
     app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
 
 if __name__ == "__main__":
